80351b82d0f8152aae9e7182c3f88dcc8b7b58c7
Mathias Gout <mathias.gout@gmail.com>
2020-09-16 08:15:27 +0200

    restriction while result window is open

--- main.py
+++ main.py
@@ -8,14 +8,6 @@ from tensorflow import keras
 
 class PaintApp:
     
-    WIDTH = 400
-    HEIGHT = WIDTH
-    BONUS_HEIGHT = 30
-    BORDER_WIDTH = 2
-    IMAGE = Image.new("L", (HEIGHT, WIDTH), "black")
-    IMAGE_resized = Image.new("L", (HEIGHT, WIDTH), "black")
-    DRAW = ImageDraw.Draw(IMAGE)
-
     # Last model in the "models" folder
     DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "models")
     MODEL_NAME = os.listdir(DIR_PATH)[-1]
@@ -28,6 +20,17 @@ class PaintApp:
     def __init__(self):
 
         self.master = Tk()
+
+        # Height and width
+        screen_height = self.master.winfo_screenheight()
+        self.WIDTH, self.HEIGHT = int(screen_height * 0.4), int(screen_height * 0.4)
+        self.BONUS_HEIGHT = int(screen_height * 0.03)
+        self.BORDER_WIDTH = 2
+
+        # Image config
+        self.IMAGE = Image.new("L", (self.HEIGHT, self.WIDTH), "black")
+        self.IMAGE_resized = Image.new("L", (self.HEIGHT, self.WIDTH), "black")
+        self.DRAW = ImageDraw.Draw(self.IMAGE)
 
         # Personalisation de la 1ere fenêtre
         self.master.title("Digits Guesser")
@@ -49,7 +52,7 @@ class PaintApp:
                                  width=self.WIDTH/2+self.BORDER_WIDTH)
         
         self.validate_button = Button(self.master, text="VALIDATE", borderwidth=1, fg="green",
-                                      font=("Times", 15, "bold"), command=self.image_transformer)
+                                      font=("Times", 15, "bold"), command=self.image_transformer, state="disable")
         self.validate_button.place(x=self.WIDTH/2+self.BORDER_WIDTH,
                                    y=self.BONUS_HEIGHT+self.HEIGHT+2*self.BORDER_WIDTH, height=self.BONUS_HEIGHT,
                                    width=self.WIDTH/2+self.BORDER_WIDTH)
@@ -64,6 +67,8 @@ class PaintApp:
         
     def paint(self, event):
         """ Fonction pour dessiner """
+
+        self.validate_button.configure(state="normal")
         color = "black"
         x1, y1 = (event.x-int(self.HEIGHT/25)), (event.y-int(self.HEIGHT/25))
         x2, y2 = (event.x+int(self.HEIGHT/25)), (event.y+int(self.HEIGHT/25))
@@ -72,12 +77,17 @@ class PaintApp:
         
     def delete(self):
         """ Fonction pour supprimer le dessin et réinitialiser l'image en mémoire """
+
+        # disable validate button
+        self.validate_button.configure(state="disable")
+        
         self.c.delete("all")
         self.IMAGE = Image.new("L", (self.WIDTH, self.HEIGHT), "black")
         self.DRAW = ImageDraw.Draw(self.IMAGE)
         
     def image_transformer(self):
         """ Fonction pour transformer l'image dessinée en image 28x28 utilisable comme input """
+
         # On centre l'image
         boundingbox = self.IMAGE.getbbox()
         image_tmp = self.IMAGE.crop(boundingbox)
@@ -100,59 +110,65 @@ class PaintApp:
     def prediction(self):
         """ Fonction qui donne la prédiction du chiffre dessiné """
         
+        # disable remove and activate button + paint
+        self.validate_button.configure(state="disable")
+        self.remove_button.configure(state="disable")
+        self.c.unbind("<B1-Motion>")
+
         # Création de la nouvelle fenêtre
-        result_window = Tk()
-        result_window.title("Prediction !")
-        result_window.geometry("{}x{}+{}+{}".format(self.WIDTH, self.HEIGHT, int(2.2*self.WIDTH), int(0.5*self.HEIGHT)))
-        result_window.minsize(self.WIDTH, self.HEIGHT)
-        result_window.maxsize(self.WIDTH, self.HEIGHT)
-        result_window.config(bg="#D8EEED")
+        self.result_window = Tk()
+        self.result_window.protocol("WM_DELETE_WINDOW", self._state_normal)
+        self.result_window.title("Prediction !")
+        self.result_window.geometry("{}x{}+{}+{}".format(self.WIDTH, self.HEIGHT, int(2.2*self.WIDTH), int(0.5*self.HEIGHT)))
+        self.result_window.minsize(self.WIDTH, self.HEIGHT)
+        self.result_window.maxsize(self.WIDTH, self.HEIGHT)
+        self.result_window.config(bg="#D8EEED")
         
         # Affichage de la prédiction
         pred = self.model.predict(self.IMAGE_resized)
         
-        message = Label(result_window,
+        message = Label(self.result_window,
                         text="You have drawn a :",
                         font=("Helvetica", int(self.HEIGHT/19), "bold"),  bg="#D8EEED")
         message.place(x=0, y=self.HEIGHT/28, height=self.HEIGHT/10, width=self.WIDTH)
         
-        pred_mes = Label(result_window,
+        pred_mes = Label(self.result_window,
                          text=pred[0].argmax(),
                          font=("Helvetica", int(self.HEIGHT/4), "bold"), bg="#D8EEED", fg="red")
         pred_mes.place(x=0, y=self.HEIGHT/5, height=self.HEIGHT/3, width=self.WIDTH)
         
         # Affichage des probas
-        mes_pred_prob = Label(result_window,
+        mes_pred_prob = Label(self.result_window,
                               text="Predicted proba :",
                               font=("Helvetica", int(self.HEIGHT/23), "underline bold"), bg="#D8EEED", anchor="w")
         mes_pred_prob.place(x=0, y=self.HEIGHT/1.7, height=self.HEIGHT/10, width=self.WIDTH/2)
         
-        first_prob = Label(result_window,
+        first_prob = Label(self.result_window,
                            text="{0} : {1:.{2}f}".format((-pred).argsort()[0][0], pred[0][(-pred).argsort()[0][0]], 3),
                            font=("Helvetica", int(self.HEIGHT/28), "bold"), bg="#D8EEED", anchor="w", fg="red")
         first_prob.place(x=self.WIDTH/8, y=self.HEIGHT/1.4, height=self.HEIGHT/15, width=3*self.WIDTH/8)
         
-        second_prob = Label(result_window,
+        second_prob = Label(self.result_window,
                             text="{0} : {1:.{2}f}".format((-pred).argsort()[0][1], pred[0][(-pred).argsort()[0][1]], 3),
                             font=("Helvetica", int(self.HEIGHT/28), "bold"), bg="#D8EEED", anchor="w")
         second_prob.place(x=5*self.WIDTH/8, y=self.HEIGHT/1.4, height=self.HEIGHT/15, width=3*self.WIDTH/8)
         
-        third_prob = Label(result_window,
+        third_prob = Label(self.result_window,
                            text="{0} : {1:.{2}f}".format((-pred).argsort()[0][2], pred[0][(-pred).argsort()[0][2]], 3),
                            font=("Helvetica", int(self.HEIGHT/28), "bold"), bg="#D8EEED", anchor="w")
         third_prob.place(x=self.WIDTH/8, y=self.HEIGHT/1.25, height=self.HEIGHT/15, width=3*self.WIDTH/8)
         
-        fourth_prob = Label(result_window,
+        fourth_prob = Label(self.result_window,
                             text="{0} : {1:.{2}f}".format((-pred).argsort()[0][3], pred[0][(-pred).argsort()[0][3]], 3),
                             font=("Helvetica", int(self.HEIGHT/28), "bold"), bg="#D8EEED", anchor="w")
         fourth_prob.place(x=5*self.WIDTH/8, y=self.HEIGHT/1.25, height=self.HEIGHT/15, width=3*self.WIDTH/8)
         
-        fifth_prob = Label(result_window,
+        fifth_prob = Label(self.result_window,
                            text="{0} : {1:.{2}f}".format((-pred).argsort()[0][4], pred[0][(-pred).argsort()[0][4]], 3),
                            font=("Helvetica", int(self.HEIGHT/28), "bold"), bg="#D8EEED", anchor="w")
         fifth_prob.place(x=self.WIDTH/8, y=self.HEIGHT/1.13, height=self.HEIGHT/15, width=3*self.WIDTH/8)
         
-        sixth_prob = Label(result_window,
+        sixth_prob = Label(self.result_window,
                            text="{0} : {1:.{2}f}".format((-pred).argsort()[0][5], pred[0][(-pred).argsort()[0][5]], 3),
                            font=("Helvetica", int(self.HEIGHT/28), "bold"), bg="#D8EEED", anchor="w")
         sixth_prob.place(x=5*self.WIDTH/8, y=self.HEIGHT/1.13, height=self.HEIGHT/15, width=3*self.WIDTH/8)
@@ -160,9 +176,20 @@ class PaintApp:
         # Reinitialisation au cas ou plusieur VALIDATE avant un REMOVE
         self.IMAGE_resized = Image.new("L", (self.HEIGHT, self.WIDTH), "black")
         self.IMAGE = Image.new("L", (self.HEIGHT, self.WIDTH), "black")
+        
+        self.result_window.mainloop()
 
-        result_window.mainloop()
+    def _state_normal(self):
+        """ Enable validate button + paint """
 
+        self.remove_button.configure(state="normal")
+        
+        # delete draw
+        self.c.delete("all")
+        self.IMAGE = Image.new("L", (self.WIDTH, self.HEIGHT), "black")
+        self.DRAW = ImageDraw.Draw(self.IMAGE)
+        self.c.bind("<B1-Motion>", self.paint)
+        self.result_window.destroy()
 
 if __name__ == "__main__":
     paint = PaintApp()
